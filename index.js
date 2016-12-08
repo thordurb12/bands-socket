5c5b8f2ee7543831d64605c3122d12a56bbfbb8a
Þórður Hans Baldursson <thordurhans@gmail.com>
2016-12-08 11:39:23 +0000

    Try moving database stuff to main index.j

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var sass = require('node-sass');
 var routes = require('./routes/routes');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/bands';
 const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/bands';
+
+const client = new pg.Client(connectionString);
+client.connect();
+
+const query1 = client.query(
+  'CREATE TABLE highscores(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, score INTEGER not null, date_added TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW());');
+query1.on('end', () => { client.end(); });
+
+const query2 = client.query('CREATE TABLE artists(id SERIAL PRIMARY KEY, uri VARCHAR(255) UNIQUE, name VARCHAR(255), count INTEGER DEFAULT 0);')
+query2.on('end', () => { client.end(); });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
