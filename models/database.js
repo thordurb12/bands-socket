4b9fbc6337dd26d969b9a30f8787f23b374dbd65
Þórður Hans Baldursson <thordurhans@gmail.com>
2016-12-08 11:14:49 +0000

    Fix database setup

--- models/database.js
+++ models/database.js
@@ -5,7 +5,7 @@ const client = new pg.Client(connectionString);
 client.connect();
 
 const query1 = client.query(
-  'CREATE TABLE highscores(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, score INTEGER not null, date_added current_date default NULL)');
+  'CREATE TABLE highscores(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, score INTEGER not null, date_added TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW());');
 query1.on('end', () => { client.end(); });
 
 const query2 = client.query('CREATE TABLE artists(id SERIAL PRIMARY KEY, uri VARCHAR(255) UNIQUE, name VARCHAR(255), count INTEGER DEFAULT 0);')
