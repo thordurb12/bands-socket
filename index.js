5c28b647e3dc89c8f66344848bda6d07c396cf48
Þórður Hans Baldursson <thordurhans@gmail.com>
2016-12-08 13:59:11 +0000

    Test

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ var routes = require('./routes/routes');
 const connectionString = process.env.DATABASE_URL || 'postgres://sdzylqidfjunid:FLDP-SOZg43cpRY-s3wwRTn137@ec2-46-137-97-169.eu-west-1.compute.amazonaws.com:5432/d5cquq4ebk7477';
 const pg = require('pg');
 
+const client = new pg.Client(connectionString);
+client.connect();
+
+const query1 = client.query(
+  'CREATE TABLE highscores(id SERIAL PRIMARY KEY, name VARCHAR(40) not null, score INTEGER not null, date_added TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW());');
+query1.on('end', () => { client.end(); });
+
+const query2 = client.query('CREATE TABLE artists(id SERIAL PRIMARY KEY, uri VARCHAR(255) UNIQUE, name VARCHAR(255), count INTEGER DEFAULT 0);')
+query2.on('end', () => { client.end(); });
+
+
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
